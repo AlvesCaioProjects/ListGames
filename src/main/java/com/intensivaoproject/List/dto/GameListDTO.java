2d220b6f66a442d4f29659abbf0923a327653a3e
Caio Alves <caioanegocios@gmail.com>
2023-07-13 13:00:19 -0300

    GET lists

---
+++ src/main/java/com/intensivaoproject/List/dto/GameListDTO.java
@@ -0,0 +1,25 @@
+package com.intensivaoproject.List.dto;
+
+import com.intensivaoproject.List.entities.GameList;
+
+public class GameListDTO {
+
+	private Long id;
+	private String name;
+	
+	public GameListDTO() {
+	}
+
+	public GameListDTO(GameList entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
---
+++ src/main/java/com/intensivaoproject/List/services/GameListService.java
@@ -0,0 +1,24 @@
+package com.intensivaoproject.List.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.intensivaoproject.List.dto.GameListDTO;
+import com.intensivaoproject.List.entities.GameList;
+import com.intensivaoproject.List.repositories.GameListRepository;
+
+@Service
+public class GameListService {
+
+	@Autowired
+	private GameListRepository gameListRepository;
+	
+	@Transactional(readOnly = true)
+	public List<GameListDTO> findAll() {
+		List<GameList> result = gameListRepository.findAll();
+		return result.stream().map(x -> new GameListDTO(x)).toList();
+	}
+}
